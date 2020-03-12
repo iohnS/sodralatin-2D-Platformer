b2cdaab278929e67bf189766799aaa3e5970c7de
iohnS <john02sun@gmail.com>
2020-03-12 20:03:18 +0100

    Add original project

---
+++ 2D Platformer/Assets/Scripts/Enemy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public Animator animator;
+
+    public int maxHealth = 100;
+    int currentHealth;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+
+    }
+
+    public void TakeDamage (int damage)
+	{
+        currentHealth -= damage;
+
+        //Play hurt animation
+
+        animator.SetTrigger("Hurt");
+
+
+
+        if (currentHealth <= 0 )
+		{
+            Die();
+
+		}
+	}
+
+    void Die()
+	{
+
+        Debug.Log("Enemy died)");
+
+
+        //Die animation
+
+        animator.SetBool("IsDead", true);
+
+
+        //Disable enemy = dissable this script
+
+        //GetComponent<CircleCollider2D>().enabled = false; 
+        //GetComponent<Seeker>().enabled = false;
+        //GetComponent<Rigidbody2D>().enabled = false;
+
+        GetComponent<Rigidbody2D>().isKinematic = true;
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        GetComponent<EnemyAI1>().enabled = false;
+
+        this.enabled = false;
+
+
+	}
+}
---
+++ 2D Platformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public Animator animator;
+
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    bool jump = false;
+
+    bool crouch = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Input from our player
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("Isjumping", true);
+
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+
+        }
+
+    }
+
+   public void OnLanding()
+    {
+        animator.SetBool("Isjumping", false);
+
+    }
+
+    //Use the input to move our character
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+
+    }
+}
